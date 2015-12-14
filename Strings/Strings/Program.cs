af54c13822519b3ac2f7027c119cadba6c6b49ce
Kosyura Olga <olyakosyura@gmail.com>
2015-12-14 17:28:48 +0300

    Strings
    -input s
    -input two symbols
    -output count of first and second symbols

---
+++ Strings/Strings/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Strings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s;
+            Console.WriteLine("Input string: ");
+            s = Console.ReadLine();
+            string a, b;
+            Console.Write("Input first symbol: ");
+            a = Console.ReadLine();
+            Console.Write("Input second symbol: ");
+            b = Console.ReadLine();
+            int acount, bcount, i, l;
+            l = s.Length;
+            acount = 0;
+            bcount = 0;
+            for (i = 0; i<l; i++)
+            {
+                if (s[i] == a[0]){acount++;}
+                if (s[i] == b[0]){bcount++;}
+            
+            }
+
+            Console.WriteLine(a+'='+(acount.ToString()));
+            Console.WriteLine(b+'='+(bcount.ToString()));
+            Console.ReadKey();
+        }
+    }
+}
